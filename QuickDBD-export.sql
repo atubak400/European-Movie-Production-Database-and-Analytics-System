118d7362b13c6aada95ded99d68b70df62e321fd
atubak400 <atubak40@gmail.com>
2023-10-23 22:50:57 +0100

    Update QuickDBD-export.sql

--- QuickDBD-export.sql
+++ QuickDBD-export.sql
@@ -2,86 +2,84 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE `ProductionCompany` (
-    `CompanyName` string  NOT NULL ,
-    `Address` string  NOT NULL ,
-    `ZIPCode` string  NOT NULL ,
-    `City` string  NOT NULL ,
-    `Nation` string  NOT NULL ,
-    `TypeOfOrganization` string  NOT NULL ,
-    `NumEmployees` int  NOT NULL ,
-    `NetValue` decimal  NOT NULL ,
-    `RegistrationDate` date  NOT NULL ,
-    `RegistrationOffice` string  NOT NULL ,
-    `RegistrationFee` decimal  NOT NULL ,
-    PRIMARY KEY (
-        `CompanyName`
-    )
+CREATE TABLE ProductionCompany (
+    CompanyName Text NOT NULL,
+    Address Text NOT NULL,
+    ZIPCode Text NOT NULL,
+    City Text NOT NULL,
+    Nation Text NOT NULL,
+    TypeOfOrganization Text NOT NULL,
+    NumEmployees Number NOT NULL,
+    NetValue Currency NOT NULL,
+    RegistrationDate Date NOT NULL,
+    RegistrationOffice Text NOT NULL,
+    RegistrationFee Currency NOT NULL,
+    PRIMARY KEY (CompanyName)
 );
 
-CREATE TABLE `Employee` (
-    `EmployeeID` int  NOT NULL ,
-    `GivenName` string  NOT NULL ,
-    `Surname` string  NOT NULL ,
-    `MiddleName` string  NOT NULL ,
-    `DateOfBirth` date  NOT NULL ,
-    `CommencementDate` date  NOT NULL ,
-    `EmailAddress` string  NOT NULL ,
-    `CompanyName` string  NOT NULL ,
-    PRIMARY KEY (
-        `EmployeeID`
-    )
+CREATE TABLE Employee (
+    EmployeeID AutoNumber PRIMARY KEY,
+    GivenName Text NOT NULL,
+    Surname Text NOT NULL,
+    MiddleName Text NOT NULL,
+    DateOfBirth Date NOT NULL,
+    CommencementDate Date NOT NULL,
+    EmailAddress Text NOT NULL,
+    CompanyName Text NOT NULL,
+    FOREIGN KEY (CompanyName) REFERENCES ProductionCompany(CompanyName)
 );
 
-CREATE TABLE `Film` (
-    `MovieCode` string  NOT NULL ,
-    `Title` string  NOT NULL ,
-    `FirstReleaseYear` int  NOT NULL ,
-    PRIMARY KEY (
-        `MovieCode`
-    )
+CREATE TABLE Film (
+    MovieCode Text NOT NULL,
+    Title Text NOT NULL,
+    FirstReleaseYear Number NOT NULL,
+    PRIMARY KEY (MovieCode)
 );
 
-CREATE TABLE `Grant` (
-    `GrantTitle` string  NOT NULL ,
-    `FundingOrganization` string  NOT NULL ,
-    `MaxMonetaryValue` decimal  NOT NULL ,
-    `DeadlineForSubmission` date  NOT NULL ,
-    PRIMARY KEY (
-        `GrantTitle`
-    )
+CREATE TABLE Grant (
+    GrantTitle Text NOT NULL,
+    FundingOrganization Text NOT NULL,
+    MaxMonetaryValue Currency NOT NULL,
+    DeadlineForSubmission Date NOT NULL,
+    PRIMARY KEY (GrantTitle)
 );
 
-CREATE TABLE `Crew` (
-    `Role` string  NOT NULL ,
-    `RoleDescription` string  NOT NULL ,
-    `Bonus` decimal  NOT NULL ,
-    `EmployeeID` int  NOT NULL ,
-    `MovieCode` string  NOT NULL 
+CREATE TABLE Crew (
+    Role Text NOT NULL,
+    RoleDescription Text NOT NULL,
+    Bonus Currency NOT NULL,
+    EmployeeID AutoNumber NOT NULL,
+    MovieCode Text NOT NULL,
+    FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID),
+    FOREIGN KEY (MovieCode) REFERENCES Film(MovieCode)
 );
 
-CREATE TABLE `Staff` (
-    `Department` string  NOT NULL ,
-    `Building` string  NOT NULL ,
-    `MonthlyWage` decimal  NOT NULL ,
-    `WorkingHours` string  NOT NULL ,
-    `CompanyName` string  NOT NULL 
+CREATE TABLE Staff (
+    Department Text NOT NULL,
+    Building Text NOT NULL,
+    MonthlyWage Currency NOT NULL,
+    WorkingHours Text NOT NULL,
+    CompanyName Text NOT NULL,
+    FOREIGN KEY (CompanyName) REFERENCES ProductionCompany(CompanyName)
 );
 
-CREATE TABLE `GrantApplication` (
-    `ApplicationDate` date  NOT NULL ,
-    `DesiredAmount` decimal  NOT NULL ,
-    `Outcome` string  NOT NULL ,
-    `GrantTitle` string  NOT NULL ,
-    `CompanyName` string  NOT NULL 
+CREATE TABLE GrantApplication (
+    ApplicationDate Date NOT NULL,
+    DesiredAmount Currency NOT NULL,
+    Outcome Text NOT NULL,
+    GrantTitle Text NOT NULL,
+    CompanyName Text NOT NULL,
+    FOREIGN KEY (GrantTitle) REFERENCES Grant(GrantTitle),
+    FOREIGN KEY (CompanyName) REFERENCES ProductionCompany(CompanyName)
 );
 
-CREATE TABLE `ContactInformation` (
-    `TelephoneNumber` string  NOT NULL ,
-    `TelephoneNumberDescription` string  NOT NULL ,
-    `NationalInsuranceNumber` string  NOT NULL ,
-    `PassportNumber` string  NOT NULL ,
-    `EmployeeID` int  NOT NULL 
+CREATE TABLE ContactInformation (
+    TelephoneNumber Text NOT NULL,
+    TelephoneNumberDescription Text NOT NULL,
+    NationalInsuranceNumber Text NOT NULL,
+    PassportNumber Text NOT NULL,
+    EmployeeID AutoNumber NOT NULL,
+    FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID)
 );
 
 ALTER TABLE `Employee` ADD CONSTRAINT `fk_Employee_CompanyName` FOREIGN KEY(`CompanyName`)
